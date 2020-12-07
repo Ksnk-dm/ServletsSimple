cc34f9e8f0cf1e49522fbe5c073d44b3fa728148
Ksnk-dm <ukrkosenko@gmail.com>
2020-12-07 14:18:47 +0200

    servlets

---
+++ src/Servlet.java
@@ -0,0 +1,54 @@
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Servlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String name = request.getParameter("name1");
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		Date date = new Date();
+		SimpleDateFormat sfd = new SimpleDateFormat("HH:mm");
+		PrintWriter pw = response.getWriter();
+		
+		
+		pw.println("<b>Hello " + name + " " + "Время сейчас: " + sfd.format(date));
+		pw.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
